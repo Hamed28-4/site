6a3cd0e0393e9c33dfe941bd91371eb8a977085f
Hamed <moeuhane2001@gmail.com>
2024-04-01 14:35:22 +0330

    fix: time field fixes

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -5,7 +5,7 @@ class Post(models.Model):
     title = models.CharField(max_length=40)
     description = models.TextField()
     date = models.DateField(auto_now_add=True)
-    time = models.DateField(auto_now_add=True)
+    time = models.TimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
