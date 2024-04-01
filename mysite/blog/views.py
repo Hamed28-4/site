2a4408b1b2fe8e076215a89e3e947539cd4266db
Hamed <moeuhane2001@gmail.com>
2024-04-01 23:18:53 +0330

    style: html files for posts

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
+
 
 def home(request):
     return HttpResponse("Welcome to my blog")
+
+def postlist(request):
+    post = Post.objects.all()
+    context = {"post": post}
+    return render(request, 'post_list.html', context)
+
+def postdetail(request, postid):
+    post = Post.objects.get(id=postid)
+    context = {"post": post}
+    return render(request, 'post_details.html', context)
