c2c63b5264d3c853c13093eed735c7ad8b407c9d
Hamed <moeuhane2001@gmail.com>
2024-04-03 23:46:06 +0330

    style: css files add

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,12 +4,14 @@ from .models import Post
 
 
 def home(request):
-    return HttpResponse("Welcome to my blog")
+    return HttpResponse("Home Page")
+
 
 def postlist(request):
     post = Post.objects.all()
     context = {"post": post}
     return render(request, 'post_list.html', context)
+
 
 def postdetail(request, postid):
     post = Post.objects.get(id=postid)
