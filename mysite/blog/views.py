0a20f5b5d893d9850859ca770e3d59069eca52d0
Hamed <moeuhane2001@gmail.com>
2024-05-04 10:25:42 +0330

    fix: remains problems fixes

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from .forms import AccountForm
-
 
 async def method_name():
     pass
