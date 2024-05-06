ad2ceda42a4462bd592724beb298441f726d8f78
Hamed <moeuhane2001@gmail.com>
2024-05-06 22:55:20 +0330

    feat: email sender sets

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -117,8 +117,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+# send_mail(subject, message, sender, [recipients], fail_silently = False)
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'moeuhane2001@gmail.com'
+EMAIL_HOST_PASSWORD = 'September/11/2001'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
